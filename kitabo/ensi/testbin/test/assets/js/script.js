588e1fb3cfd33c29f7ca2e9598e800506eadda2e
abikesa <abikesa.sh@gmail.com>
2025-04-24 09:37:37 -0400

    kitabo/ensi/testbin/test/assets/js/script.js

--- kitabo/ensi/testbin/test/assets/js/script.js
+++ kitabo/ensi/testbin/test/assets/js/script.js
@@ -203,3 +203,4 @@ window.addEventListener('load', function() {
 document.getElementById("calculate-risk-button").addEventListener("click", function() {
     throttledCalculateRisk();
 });
+# flick 20250424133737-BqOn
