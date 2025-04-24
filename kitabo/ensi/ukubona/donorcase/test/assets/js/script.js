0237a1fa2e9aecdb7473c073bcc380509c5eac5a
abikesa <abikesa.sh@gmail.com>
2025-04-23 21:53:20 -0400

    kitabo/ensi/ukubona/donorcase/test/assets/js/script.js

--- kitabo/ensi/ukubona/donorcase/test/assets/js/script.js
+++ kitabo/ensi/ukubona/donorcase/test/assets/js/script.js
@@ -251,3 +251,4 @@ document.getElementById("calculate-risk-button").addEventListener("click", funct
 # flick 20250423021929-EAUK
 # flick 20250423195957-Us1x
 # flick 20250423204915-i1NR
+# flick 20250424015320-b8Rl
